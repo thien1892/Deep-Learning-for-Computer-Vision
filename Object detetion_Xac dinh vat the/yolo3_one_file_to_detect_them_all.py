af56373e97bdf08f7c00e2a479f4abd732dcae17
thien1892 <thien1892@gmail.com>
2021-10-07 23:16:48 +0700

    Update yolo3_one_file_to_detect_them_all.py

--- Object detetion_Xac dinh vat the/yolo3_one_file_to_detect_them_all.py
+++ Object detetion_Xac dinh vat the/yolo3_one_file_to_detect_them_all.py
@@ -381,57 +381,57 @@ def draw_boxes(image, boxes, labels, obj_thresh):
         
     return image      
 
-def _main_(args):
-    weights_path = args.weights
-    image_path   = args.image
+# def _main_(args):
+#     weights_path = args.weights
+#     image_path   = args.image
 
-    # set some parameters
-    net_h, net_w = 416, 416
-    obj_thresh, nms_thresh = 0.5, 0.45
-    anchors = [[116,90,  156,198,  373,326],  [30,61, 62,45,  59,119], [10,13,  16,30,  33,23]]
-    labels = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", \
-              "boat", "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", \
-              "bird", "cat", "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", \
-              "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", \
-              "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard", \
-              "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", \
-              "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", \
-              "chair", "sofa", "pottedplant", "bed", "diningtable", "toilet", "tvmonitor", "laptop", "mouse", \
-              "remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator", \
-              "book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"]
+#     # set some parameters
+#     net_h, net_w = 416, 416
+#     obj_thresh, nms_thresh = 0.5, 0.45
+#     anchors = [[116,90,  156,198,  373,326],  [30,61, 62,45,  59,119], [10,13,  16,30,  33,23]]
+#     labels = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", \
+#               "boat", "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", \
+#               "bird", "cat", "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", \
+#               "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", \
+#               "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard", \
+#               "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", \
+#               "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", \
+#               "chair", "sofa", "pottedplant", "bed", "diningtable", "toilet", "tvmonitor", "laptop", "mouse", \
+#               "remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator", \
+#               "book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"]
 
-    # make the yolov3 model to predict 80 classes on COCO
-    yolov3 = make_yolov3_model()
+#     # make the yolov3 model to predict 80 classes on COCO
+#     yolov3 = make_yolov3_model()
 
-    # load the weights trained on COCO into the model
-    weight_reader = WeightReader(weights_path)
-    weight_reader.load_weights(yolov3)
+#     # load the weights trained on COCO into the model
+#     weight_reader = WeightReader(weights_path)
+#     weight_reader.load_weights(yolov3)
 
-    # preprocess the image
-    image = cv2.imread(image_path)
-    image_h, image_w, _ = image.shape
-    new_image = preprocess_input(image, net_h, net_w)
+#     # preprocess the image
+#     image = cv2.imread(image_path)
+#     image_h, image_w, _ = image.shape
+#     new_image = preprocess_input(image, net_h, net_w)
 
-    # run the prediction
-    yolos = yolov3.predict(new_image)
-    boxes = []
+#     # run the prediction
+#     yolos = yolov3.predict(new_image)
+#     boxes = []
 
-    for i in range(len(yolos)):
-        # decode the output of the network
-        boxes += decode_netout(yolos[i][0], anchors[i], obj_thresh, nms_thresh, net_h, net_w)
+#     for i in range(len(yolos)):
+#         # decode the output of the network
+#         boxes += decode_netout(yolos[i][0], anchors[i], obj_thresh, nms_thresh, net_h, net_w)
 
-    # correct the sizes of the bounding boxes
-    correct_yolo_boxes(boxes, image_h, image_w, net_h, net_w)
+#     # correct the sizes of the bounding boxes
+#     correct_yolo_boxes(boxes, image_h, image_w, net_h, net_w)
 
-    # suppress non-maximal boxes
-    do_nms(boxes, nms_thresh)     
+#     # suppress non-maximal boxes
+#     do_nms(boxes, nms_thresh)     
 
-    # draw bounding boxes on the image using labels
-    draw_boxes(image, boxes, labels, obj_thresh) 
+#     # draw bounding boxes on the image using labels
+#     draw_boxes(image, boxes, labels, obj_thresh) 
  
-    # write the image with bounding boxes to file
-    cv2.imwrite(image_path[:-4] + '_detected' + image_path[-4:], (image).astype('uint8')) 
+#     # write the image with bounding boxes to file
+#     cv2.imwrite(image_path[:-4] + '_detected' + image_path[-4:], (image).astype('uint8')) 
 
-if __name__ == '__main__':
-    args = argparser.parse_args()
-    _main_(args)
+# if __name__ == '__main__':
+#     args = argparser.parse_args()
+#     _main_(args)
